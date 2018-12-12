8efff979cae5f66834f6534d98b522592d23011f
trevino <rafael.trevino@me.com>
2018-12-12 18:21:09 -0500

    finished js logi

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,59 +1,56 @@
 $(document).ready(function() {
 
 var adjectives = ["time", "dance", "year", "way", "day", "thing", "world", "life", "hand", "child"];
-var rowCount = 1;
-var gifCount = 0;
-
-// Remember to add alt text to each GIF (use information inside response object)
+var state = $(this).attr("data-state");
+var rowID = -1;
+var images = [];
+var catAdj;
 
 function displayCatGifs(catAdj) {
 
-    catAdj = $(this).attr("data-name");  
     var APIkey = "b98xRER1URXt0Nhz68BEVXWnfI43okvO";
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + catAdj + "+black+white" + "&api_key=" + APIkey + "&limit=10"
 
     $.ajax({
         url: queryURL,
         method: "GET",
-        // accept: "image/*",
     }).then(function(response) {
 
         for (i=0; i < response.data.length; i++) {
-
             var catGif = $("<div>").addClass("col-md-3");
             var rating = $("<p>").html(response.data[i].rating);
+            var altText = $(response.data[i].title);
             var img = $("<img>");
-            img.attr("src", response.data[i].images.fixed_height_still.url);
+            var imgSrc = response.data[i].images.fixed_height_still.url;
+            var imgStill = response.data[i].images.fixed_height_still.url;
+            var imgAnimate = response.data[i].images.fixed_height.url;
+            img.attr("src", imgSrc).attr("alt", altText);
             img.attr("data-id", response.data[i].id);
-    
+            img.attr("data-still", imgStill);
+            img.attr("data-animate", imgAnimate);
+            img.attr("data-state", "still");
+            img.addClass("gif");
             catGif.append(rating, img);
+            images.push(catGif);
+        };
 
-            if (i < 3) {
-                var newRow = $("<div>").addClass("row");
-                newRow.attr("id", "first");
-                $("#displayGifsDiv").append(newRow);
-            } else if (i >= 3 && i < 7) {
-                var newRow = $("<div>").addClass("row");
-                newRow.attr("data-id", "second");
-                $("#displayGifsDiv").append(newRow);
-                newRow.attr("second").prepend(catGif);
-            } else if (i >= 7) {
-                var newRow = $("<div>").addClass("row");
-                newRow.attr("data-id", "third");
-                $("#displayGifsDiv").append(newRow);
-                newRow.attr("third").prepend(catGif);
+        $("#displayGifsDiv").empty();
+
+        for (i = images.length - 1; i >= 0; i--) {
+            if ((i%4)===0) {
+                rowID++;
+                var rowDiv = $("<div>").addClass("row").attr("id", rowID);
+                $("#displayGifsDiv").append(rowDiv);
+                rowDiv.prepend(images[i]);
+            } else {
+                findRow = "#" + rowID;
+                $(findRow).prepend(images[i]);
             }
-        
-        $("#first").prepend(catGif);
-        
-         }
-    
-    
+        }
     });
-    
-}
+};
 
-
+// Create buttons
 function renderButtons() {
     $("#buttonsDiv").empty();
     for (var i=0; i < adjectives.length; i++) {
@@ -65,7 +62,7 @@ function renderButtons() {
     }
 }
 
-// User input
+// User input and show GIFs
 $("#select-catadj").on("click", function(event) {
     event.preventDefault();
     var catAdj = $("#gif-input").val();
@@ -74,8 +71,26 @@ $("#select-catadj").on("click", function(event) {
     displayCatGifs(catAdj);
 })
 
-// Buttons
-$(document).on("click", ".adjective", displayCatGifs);
+// Button click and show GIFs
+$(this).on("click", ".adjective", function() {
+    var catAdj = $(this).attr("data-name");
+    displayCatGifs(catAdj);
+});
+
+// Animate and pause on click
+$(this).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    var animatedURL = $(this).attr("data-animate");
+    var stillURL = $(this).attr("data-still");
+    if (state === "still") {
+      $(this).attr("src", animatedURL);
+      $(this).attr("data-state", "animated");
+    } else if (state === "animated") {
+      $(this).attr("src", stillURL);
+      $(this).attr("data-state", "still");
+    }
+});
+
 renderButtons();
 
 }); // End of document ready function
